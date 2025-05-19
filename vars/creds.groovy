a49c58e34cc74a13daed0cbd19d69006fc407dcd
harshit805 <86709950+harshit805@users.noreply.github.com>
2025-05-19 18:32:04 +0530

    Update creds.groovy

--- vars/creds.groovy
+++ vars/creds.groovy
@@ -1,9 +1,6 @@
 def creds(){
-  withCredentials([usernamePassword(
+   withCredentials([usernamePassword(
                     'credentialsId':"dockerhubcreds", 
                     passwordVariable:"dockerHubPass", 
-                    usernameVariable:"dockerHubUser")]){
-                sh "docker login -u ${env.dockerHubUser} -p ${env.dockerHubPass}"        
-                sh "docker image tag notes-app:latest harrycreations/notes-app:latest"
-                sh "docker push ${env.dockerHubUser}/notes-app:latest"
+                    usernameVariable:"dockerHubUser")])
   }
