8f77a2082c52971447ecd6d61e244d476a87c770
harshit805 <86709950+harshit805@users.noreply.github.com>
2025-05-20 02:12:56 +0530

    Update docker_build.groovy

--- vars/docker_build.groovy
+++ vars/docker_build.groovy
@@ -1,6 +1,6 @@
 def call(String ProjectName, String ImageTag, String DockerHubUser) {
     def image = docker.build("${DockerHubUser}/${ProjectName}:${ImageTag}")
-    docker.withRegistry('https://index.docker.io/v1/', 'dockerhub-creds') {
+    docker.withRegistry('https://index.docker.io/v1/', 'dockerhubcreds') {
         image.push()
     }
 }
